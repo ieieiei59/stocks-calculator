446ac5128ca8112e56dc4e29b408f4dc479c57fb
ieieiei59 <yota0509a@gmail.com>
2023-10-24 15:56:04 +0000

    ルート定義を作成

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,9 +1,25 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
+import { render } from "solid-js/web";
+import { Route, Router, Routes } from "@solidjs/router";
 
-import './index.css'
-import App from './App'
+import "./index.css";
+import App from "./App";
 
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
-render(() => <App />, root!)
+render(
+  () => (
+    <Router>
+      <Routes>
+        <Route path="/" component={App} />
+        <Route path="/pallet">
+          <Route path="/calculator" component={App}></Route>
+        </Route>
+        <Route path="/attendance">
+          <Route path="/hour-calculator" component={App} />
+        </Route>
+      </Routes>
+    </Router>
+  ),
+  root!
+);
